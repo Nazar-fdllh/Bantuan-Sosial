a0c5f051c163452d43b3a132ffed499d63949678
Nazar-fdllh <muhammadnazarfadillah@gmail.com>
2024-12-21 23:15:59 +0800

    style done

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -17,17 +17,10 @@
     </style>
 </head>
 <body>
-    <header class="bg-dark text-white py-3">
-        <div class="container">
-            <h2 class="mb-0">Sistem Informasi Penerima Bantuan Sosial</h2>
-        </div>
-    </header>
+    
     <main>
         @yield('content')
     </main>
-    <footer class="bg-dark text-white text-center py-3 mt-5">
-        <p>&copy; {{ date('Y') }} Sistem Informasi. All Rights Reserved.</p>
-    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
